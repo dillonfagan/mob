a8bc4aee3484a24ea3a0b143cc4a09997b56ce7c
Dillon Fagan <dillonfagan@pm.me>
2022-11-01 10:55:08 -0400

    Increase font size of text in dialog

--- lib/pages/timer/widgets/end_mob_alert.dart
+++ lib/pages/timer/widgets/end_mob_alert.dart
@@ -8,16 +8,25 @@ class EndMobAlertDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text('End Mob'),
+      title: const Text(
+        'End Mob',
+        textScaleFactor: 2,
+      ),
       actions: [
         TextButton(
           onPressed: () => Navigator.of(context).pop(),
           style: TextButton.styleFrom(foregroundColor: Colors.grey.shade200),
-          child: const Text('Cancel'),
+          child: const Text(
+            'Cancel',
+            textScaleFactor: 1.5,
+          ),
         ),
         TextButton(
           onPressed: onConfirm,
-          child: const Text('End Mob'),
+          child: const Text(
+            'End Mob',
+            textScaleFactor: 1.5,
+          ),
         ),
       ],
     );
